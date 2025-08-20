093f43124a40a946b234088981d22d6050b7a821
breelk <935549961@qq.com>
2025-08-20 18:17:35 +0800

    暂存

--- internal/app/handler/system/auth.go
+++ internal/app/handler/system/auth.go
@@ -43,7 +43,7 @@ func (h *AuthHandler) Login(c *gin.Context) {
 	)
 
 	defer func() {
-		response.HandleDefault(c, res)(&err, recover())
+		response.HandleDefault(c, response.WithData(res))(&err, recover())
 	}()
 
 	if err = c.ShouldBindJSON(&req); err != nil {
